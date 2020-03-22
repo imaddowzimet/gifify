3c11af7ec8b628bd1860083292719fefb11aa634
Isaac Maddow-Zimet <imaddowzimet@gmail.com>
2020-03-22 17:09:24 -0400

    Tring to fix unit test (really a fake test; just trying to get travis build to work)

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -2,8 +2,6 @@ context("gifify")
 
 testthat::test_that("throws error", {
   testthat::expect_error(
-    videolink <- "Langoliers clip.mov",
-    text  <- c("", "", ""),
-    gifify(videolink, text = text, cut = 70)
+    gifify("Langoliers clip.mov", text = c("", "", ""), cut = 70)
   )
 })
